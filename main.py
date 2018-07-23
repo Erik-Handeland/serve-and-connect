3d938a3f302d1b4c59e96d917ae862eb62fbed00
Erik <demouser@Eriks-MBP.local>
2018-07-23 11:09:38 -0700

    add main code

--- main.py
+++ main.py
@@ -16,22 +16,27 @@ class LocationSearch(ndb.Model):
 
 class LoginPage(webapp2.RequestHandler):
     def get(self):
-        login_template = jinja_env.get_template('login.html')
-        # self.response.write(search_template.render({"test"}))
-
-class LocationPage(webapp2.RequestHandler):
-    def get(self):
-        city = self.request.get("city")
-        state = self.request.get("state")
         location_template = jinja_env.get_template('location.html')
-        params = {"city": city,
-                "state": state,
-                }
-        api_url = "https://www.zipcodeapi.com/rest/"+ "RAU0IsJOFZxVEgTEAlllssd44GJm9euGb3gtYaAbNWxYv9f7bCFluUnLczMAA1OTr" + "/city-zips." + "json/" + city + "/" + state
+        zipcode = self.request.get("zipcode")
+        self.response.write(location_template.render(zipcode))
+        
+    def post(self):
+        login_template = jinja_env.get_template('login.html')
+        self.response.write(login_template.render())
 
-        print api_url
+
+    # def get(self):
+    #     city = self.request.get("city")
+    #     state = self.request.get("state")
+    #     location_template = jinja_env.get_template('location.html')
+    #     params = {"city": city,
+    #             "state": state,
+    #             }
+    #     api_url = city + ", " + state
+    #     api_url = "https://www.zipcodeapi.com/rest/"+ "RAU0IsJOFZxVEgTEAlllssd44GJm9euGb3gtYaAbNWxYv9f7bCFluUnLczMAA1OTr" + "/city-zips." + "json/" + city + "/" + state
+    #     self.response.write(location_template.render())
+    #     print api_url
 
 app = webapp2.WSGIApplication([
-    ('/location', LocationPage),
     ('/login', LoginPage),
 ], debug=True)
