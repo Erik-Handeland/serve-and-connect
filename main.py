1af1dcf57680066d9a4ef90aa16678eafbde0a4a
Jordan <demouser@MacBook-Pro.local>
2018-07-23 21:39:41 -0700

    added

--- main.py
+++ main.py
@@ -4,16 +4,42 @@ import os
 import json
 import urllib
 import urllib2
+from google.appengine.api import users
 
 from google.appengine.ext import ndb
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(
         os.path.dirname(__file__) + "/templates"))
+        
+@ndb.transactional
+def find_or_create_user():
+    user = users.get_current_user()
+    if user:
+        key = ndb.Key('UserInfo', user.user_id())
+        stuser = key.get()
+        if not stuser:
+            stuser = UserInfo(key=key, nickname=user.nickname())
+        stuser.put()
+        return stuser;
+    return None
+def get_log_inout_url(user):
+    if user:
+        return users.create_logout_url('/')
+    else:
+        return users.create_login_url('/')
+
 
 class IndexPage(webapp2.RequestHandler):
     def get(self):
         index_template = jinja_env.get_template('index.html')
         self.response.write(index_template.render())
+
+class PersonPage(ndb.Model):
+        print("hello world")
+        name = ndb.StringProperty(required=True)
+        email = ndb.StringProperty(required=True)
+        user_id = ndb.IntegerProperty(required=False)
+
 
 class LoginPage(webapp2.RequestHandler):
     def get(self):
